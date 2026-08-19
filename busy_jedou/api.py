036df593c874dacafb00e9d8e8242da3c5c0256b
HrnekCesky <adabab0909@gmail.com>
2026-08-19 15:57:21 +0200

    added filtering only for bus route lookup

--- busy_jedou/api.py
+++ busy_jedou/api.py
@@ -46,5 +46,5 @@ def route(place1, place2, num1, num2):
     id2 = res2[num2].get("stop_id")
     pos2 = {"name": name2, "lat": pos2lat, "lon": pos2lon, "stop_id": id2}
 
-    response = requests.get(f"{ROUTE_LOOKUP_BASE_URL}/plan", headers=HEADERS, params={"fromPlace": id1, "toPlace": id2})
+    response = requests.get(f"{ROUTE_LOOKUP_BASE_URL}/plan", headers=HEADERS, params={"fromPlace": id1, "toPlace": id2, "transitModes": "BUS"})
     return {"places": {"departure": pos1, "destination": pos2}, "routing": response.json()}
